27c287bc270e9f5b8fec88756ef930c6382b2b8d
Claus Hansen <claus@multimedion.dk>
2025-04-08 19:05:18 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # bookmarklet_vejl_karakter_sheet
-Brug denne bookmarklet til at generere et excel workbook til vejledende karakterer.
+
+Denne bookmarklet hjælper dig med hurtigt at downloade en Excel-fil med vejledende karakterer direkte fra din Moodle-deltagerliste. Når du kører bookmarkleten, oprettes et Excel-dokument med to faner (“Halvår 1” og “Halvår 2”). Hver fane indeholder en liste over elever med de følgende kolonner:
+
+- Navn
+- Dansk
+- Engelsk
+- Informatik
+- Samfundsfag
+- Markedskommunikation
+- Virksomhedsøkonomi
+- Erhvervsjura
+- Gennemsnit
+
+I kolonnen "Gennemsnit" beregnes der automatisk et gennemsnit (vises som en tom celle, hvis der ikke er indtastet data).
+
+## Sådan bruger du bookmarkleten
+
+1. **Tilføj bookmarkleten til din browser:**
+   - Download og åbn HTML-siden "instruktioner.html" med bookmarkletten.
+   - Træk knappen med teksten **"Vejledende Karakterer"** til din bogmærkelinje.
+
+2. **Forbered Moodle-siden:**
+   - Gå til oversigten med deltagere i Moodle.
+   - **Filtrér listen**, så kun de aktive elever med rollen "Elev" vises (vælg fx "Aktiv" for status og "Elev" for rolle, og klik på “Anvend filtre”).
+   - Marker de elever, du ønsker data for, ved at sætte flueben ved hver elev.
+
+3. **Kør bookmarkleten:**
+   - Klik på din bookmarklet i bogmærkelinjen.
+   - Excel-filen “Vejledende Karakterer – [År].xlsx” downloades automatisk.
+
+Det var det! Du får en klar og overskuelig Excel-fil med de nødvendige felter, der er klar til videre brug.
+
---
+++ bookmarklet_js.js
@@ -0,0 +1,79 @@
+
+require.config({
+    paths: {
+        xlsx: "https://cdn.jsdelivr.net/gh/sheetjs/sheetjs/dist/xlsx.full.min"
+    }
+});
+
+require(["xlsx"], function (XLSX) {
+    console.log('SheetJS (XLSX) loaded..');
+
+    var elever = $('#participants tbody tr:not(.emptyrow)')
+        .has('input.usercheckbox:checked')
+        .find('th.c1');
+    if (elever.length === 0) {
+        alert('Ingen elever fundet eller ingen elever valgt!');
+        return;
+    }
+
+    // Overskriftsrækken med ekstra kolonne til Gennemsnit
+    var header = ["Navn", "Dansk", "Engelsk", "Informatik", "Samfundsfag",
+        "markedskommunikation", "Virksomhedsøkonomi", "Erhvervsjura",
+        "Gennemsnit"];
+    var sheet1Data = [header];
+    var sheet2Data = [header];
+
+    elever.each(function (index) {
+        var linkElement = this.querySelector('a');
+        var fuldtNavn = "";
+        if (linkElement) {
+            fuldtNavn = Array.from(linkElement.childNodes)
+                .filter(node => node.nodeType === Node.TEXT_NODE)
+                .map(node => node.textContent.trim())
+                .join("");
+        } else {
+            fuldtNavn = $(this).text().trim();
+        }
+        // Excel-rækkenummer (den første elevrække er 2, fordi overskriften er i række 1)
+        var excelRowNum = index + 2;
+
+        // Benyt IFERROR: AVERAGE(...) → IFERROR(AVERAGE(...), "")
+        var row = [
+            fuldtNavn,
+            "", "", "", "", "", "", "",
+            { f: "IFERROR(AVERAGE(B" + excelRowNum + ":H" + excelRowNum + "), \"\")" }
+        ];
+        sheet1Data.push(row);
+        sheet2Data.push(row);
+    });
+
+    var wb = XLSX.utils.book_new();
+    var ws1 = XLSX.utils.aoa_to_sheet(sheet1Data);
+    var ws2 = XLSX.utils.aoa_to_sheet(sheet2Data);
+    XLSX.utils.book_append_sheet(wb, ws1, "Halvår 1");
+    XLSX.utils.book_append_sheet(wb, ws2, "Halvår 2");
+
+    var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+
+    function s2ab(s) {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+    }
+
+    var year = new Date().getFullYear();
+    var filename = "Vejledende Karakterer - " + year + ".xlsx";
+
+    if (typeof saveAs === "undefined") {
+        var fsScript = document.createElement('script');
+        fsScript.src = "https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js";
+        fsScript.onload = function () {
+            saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), filename);
+        };
+        document.head.appendChild(fsScript);
+    } else {
+        saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), filename);
+    }
+});
+
